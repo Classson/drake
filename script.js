f8b7830f9a46167e69174711e12b2688ba806237
Alex <Alex.mikulewicz@gmail.com>
2018-07-11 15:35:16 -0400

    caught some syntax errors

--- script.js
+++ script.js
@@ -1,85 +1,73 @@
 (function() {
-function buildQuiz(){
-    //array to store the output
+  function buildQuiz() {
+    // we'll need a place to store the HTML output
     const output = [];
-    
-    //for each question
-    myQuestions.forEach(
-        (currentQuestion, questionNumber) => {
-            //list of answer choices
-            const answers = [];
-            
-            //for each question
-            myQuestions.forEach((currentQuestion, questionNumber) => {
-                
-            //store list of answer choices
-            const answers = [];
-                
-            //for each answer
-            for(letter in currentQuestion.answers){
-                
-                //html radio button
-                answers.push(
-                `<label>
-                    <input type="radio" 
-                    name="question${questionNumber}" value="${letter}">
-                    
-                    ${letter} :
-                    ${currentQuestion.answers[letter]}
-                </label>`
-                
-                );
-            }
-            //add question and answer to output
-            output.push(
-            `<div class="question"> ${currentQuestion.question}</div>
-            <div class="answers">
-            $`{answers.join("")}</div>`
-            );
-        });
-    
-    //combine output string of html and display
+
+    // for each question...
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+      // we'll want to store the list of answer choices
+      const answers = [];
+
+      // and for each available answer...
+      for (letter in currentQuestion.answers) {
+        // ...add an HTML radio button
+        answers.push(
+          `<label>
+            <input type="radio" name="question${questionNumber}" value="${letter}">
+            ${letter} :
+            ${currentQuestion.answers[letter]}
+          </label>`
+        );
+      }
+
+      // add this question and its answers to the output
+      output.push(
+        `<div class="question"> ${currentQuestion.question} </div>
+        <div class="answers"> ${answers.join("")} </div>`
+      );
+    });
+
+    // finally combine our output list into one string of HTML and put it on the page
     quizContainer.innerHTML = output.join("");
-}
+  }
 
-function showResults(){
+  function showResults() {
+    // gather answer containers from our quiz
+    const answerContainers = quizContainer.querySelectorAll(".answers");
 
-//gather answer containers
-const answerContainers = quizContainer.querySelectorAll(".answers");
-
-//keep track of user's anaswers
+    // keep track of user's answers
     let numCorrect = 0;
 
-//for each questions
+    // for each question...
     myQuestions.forEach((currentQuestion, questionNumber) => {
-        //find selected answer
-        const answerContainer = answerContainers[questionNumber];
-        const selector = `input[name=question${questionNumber}]:checked`;
-        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-        
-        //if answer is correct
-        if(userAnswer === currentQuestion.correctAnswer) {
-            //add to the number of correct answers
-            numCorrect++;
-            
-            //color the answer green
-            
-            answerContainers[questionNumber].style.color = "lightgreen";
-        } else {
-            //if answer wrong or blank color red
-            
-            answerContainers[questionNumber].style.color = "red";
-        }
-    });
-    
-//show number of correct results out of total
-resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-}
+      // find selected answer
+      const answerContainer = answerContainers[questionNumber];
+      const selector = `input[name=question${questionNumber}]:checked`;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
 
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
-const myQuestions = [
+      // if answer is correct
+      if (userAnswer === currentQuestion.correctAnswer) {
+        // add to the number of correct answers
+        numCorrect++;
+
+        // color the answers green
+        answerContainers[questionNumber].style.color = "lightgreen";
+      } else {
+        // if answer is wrong or blank
+        // color the answers red
+        answerContainers[questionNumber].style.color = "red";
+      }
+    });
+
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+  }
+
+  const quizContainer = document.getElementById("quiz");
+  const resultsContainer = document.getElementById("results");
+  const submitButton = document.getElementById("submit");
+  const myQuestions = [
+    { 
     question: "What's your cat's name?",
     answers: {
         a: "Coach",
@@ -105,10 +93,9 @@ const myQuestions = [
     }
 ];
 
-//display quiz
-buildQuiz();
+  // display quiz right away
+  buildQuiz();
 
-//show results on submit
-
-submitButton.addEventListener(click, showResults);
+  // on submit, show results
+  submitButton.addEventListener("click", showResults);
 })();
