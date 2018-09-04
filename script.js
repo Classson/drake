a92ba0fb775baeadd5f318cd0947f220c57c1e81
Alex <Jillbk22@gmail.com>
2018-09-04 17:24:30 -0400

    started rebuilding quiz function

--- script.js
+++ script.js
@@ -41,103 +41,40 @@ Quotes
 
 
 */
-(function() {
-  function buildQuiz() {
-    // HTML output
-    const output = [];
 
-    // for each question
-    myQuestions.forEach((currentQuestion, questionNumber) => {
-      // list of answer choices
-      const answers = [];
-
-      // for each available answer
-      for (letter in currentQuestion.answers) {
-        // ...add an HTML radio button
-        answers.push(
-          `<label>
-            <input type="radio" name="question${questionNumber}" value="${letter}">
-            ${letter} :
-            ${currentQuestion.answers[letter]}
-          </label>`
-        );
-      }
-      // add this question and its answers to the output
-      output.push(
-        `<div class="question"> ${currentQuestion.question} </div>
-        <div class="answers"> ${answers.join("")} </div>`
-      );
-    });
-
-    // combine output list into string of HTML and put it on the page
-    quizContainer.innerHTML = output.join("");
-  }
-
-  function showResults() {
-    // gather answer containers from quiz
-    const answerContainers = quizContainer.querySelectorAll(".answers");
-
-    // keep track of user's answers
-    let numCorrect = 0;
-
-    // for each question
-    myQuestions.forEach((currentQuestion, questionNumber) => {
-      // find selected answer
-      const answerContainer = answerContainers[questionNumber];
-      const selector = `input[name=question${questionNumber}]:checked`;
-      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-
-      // if answer is correct
-      if (userAnswer === currentQuestion.correctAnswer) {
-        // add to the number of correct answers
-        numCorrect++;
-
-        // color the answers green
-        answerContainers[questionNumber].style.color = "lightgreen";
-      } else {
-        // if answer is wrong or blank
-        // color the answers red
-        answerContainers[questionNumber].style.color = "red";
-      }
-    });
-
-    // show number of correct answers out of total
-    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-  }
-
-  const quizContainer = document.getElementById("quiz");
-  const resultsContainer = document.getElementById("results");
-  const submitButton = document.getElementById("submit");
-  const myQuestions = [
-    { 
-    question: "What's your cat's name?",
-    answers: {
-        Drake: "Drake",
-        Emo: "Emo"
-      },
-      correctAnswer: "Drake"
+const myQuestions = [
+    {
+        question: "Next time we fuck, I don't wanna fuck, I wanna make love. Next time we talk, I don't wanna just talk, I wanna trust. Next time I stand tall I wanna be standing for you",
+        answers: {
+            a: Drake,
+            b: Emo
+    },
+        correctAnswer: 'a'
     },
     {
-    question: "What color is he?",
-    answers: {
-        a: "black",
-        b: "orange"
-      },
-      correctAnswer: "b"
-    },
-    {
-    question: "Is he a good cat?",
-    answers: {
-        a: "yes",
-        b: "no"
-      },
-      correctAnswer: "a"
+        question: "Breathe in deep, let it out slow. Did you hear it’s all my fault again? I know why no one else knows why I am all alone again.",
+        answers: {
+            a: Drake,
+            b: Emo
+        },
+        correctAnswer: 'b'
     }
-];
+]
 
-  // display quiz right away
-  buildQuiz();
-
-  // on submit, show results
-  submitButton.addEventListener("click", showResults);
-})();
+function buildQuiz (questions, quizContainer, resutlsContainer) {
+    
+    
+    function showQuestions(questions, quizContainer){
+        
+    }
+    
+    function showResults(questions, quizContainer, resutlsContainer) {
+        
+    }
+    
+    showQuestions(questions, quizContainer);
+    
+    submitButton.onclick = function (){
+        showResults(questions, quizContainer, resutlsContainer);
+    }
+}
