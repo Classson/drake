e9f17df4cf6d72394031ad965d7b29a8b4edb6f8
Alex <Jillbk22@gmail.com>
2018-09-05 16:32:04 -0400

    fixed questions so all answers display

--- script.js
+++ script.js
@@ -66,54 +66,77 @@ let resultsContainer = document.getElementById('results');
 let submitButton = document.getElementById('submit');
 
 
-function buildQuiz (questions, quizContainer, resutlsContainer) {
+function generateQuiz (questions, quizContainer, resutlsContainer) {
     
     
-    function showQuestions(questions, quizContainer){
-        let output = [];
-        let answers;
-        
-        for(let i = 0; i < questions.length; i++){
-            answers = [];
-            for(letter in questions[i].answers){
-                answers.push('<label>'
-                            + '<input type="radio" name="question' +i+ '" value="'+letter+'">'
-                            + letter + ': '
-                            + questions[i].answers[letter]
-                            + '</label>');
-            }
-            
-            output.push(
-            '<div class="question">' + questions[i].question + '</div>' + '<div class="answers>' + answers.join('') + '</div>');
-        }
-        
-        quizContainer.innerHTML = output.join('');
-        
-    }
+   function showQuestions(questions, quizContainer){
+	
+	var output = [];
+	var answers;
+
+	
+	for(var i=0; i<questions.length; i++){
+		
+		answers = [];
+
+		
+		for(letter in questions[i].answers){
+
+			
+			answers.push(
+				'<label>'
+					+ '<input type="radio" name="question'+i+'" value="'+letter+'">'
+					+ letter + ': '
+					+ questions[i].answers[letter]
+				+ '</label>'
+			);
+		}
+
+		
+		output.push(
+			'<div class="question">' + questions[i].question + '</div>'
+			+ '<div class="answers">' + answers.join('') + '</div>'
+		);
+	}
+
+	
+	quizContainer.innerHTML = output.join('');
+}
     
-    function showResults(questions, quizContainer, resutlsContainer) {
-        let answerContainers = quizContainer.querySelectorAll('.answers');
-        
-        let userAnswer = '';
-        let numCorrect = 0;
-        
-        for(let i = 0; i < questions.length; i++){
-            
-            userAnswer = (answerContainers[i].querySelector('input[name=question' +i+']:checked')|| {}).value;
-            
-            if(userAnswer === question[i].correctAnswer){
-                numCorrect++;
-                
-                answerContainers[i].style.color = 'lightgreen';
-            }
-            
-            else {
-                answerContainers[i].style.color = 'red';
-            }
-        }
-        
-        resultsContainer.innerHTML = numCorrect + ' Out of ' + questions.length;
-    }
+    
+function showResults(questions, quizContainer, resultsContainer){
+	
+	
+	var answerContainers = quizContainer.querySelectorAll('.answers');
+	
+	
+	var userAnswer = '';
+	var numCorrect = 0;
+	
+	
+	for(var i=0; i<questions.length; i++){
+
+		
+		userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+		
+		
+		if(userAnswer===questions[i].correctAnswer){
+			
+			numCorrect++;
+			
+			
+			answerContainers[i].style.color = 'lightgreen';
+		}
+		
+		else{
+			
+			answerContainers[i].style.color = 'red';
+		}
+	}
+
+	
+	resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+}
     
     showQuestions(questions, quizContainer);
     
@@ -122,5 +145,5 @@ function buildQuiz (questions, quizContainer, resutlsContainer) {
     }
 }
 
-buildQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
 
